66f515594054001788878d3051e9364be8aa06b6
Karthik Manickam <karthik.omanickam@gmail.com>
2020-02-06 19:11:57 -0800

    Updates

---
+++ precache-manifest.c17d095c22d62ddfab6c23fe0365975b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98df733c36c4f2cfba0e13ec0638388f",
+    "url": "/haus/index.html"
+  },
+  {
+    "revision": "982d2f945288aef25c73",
+    "url": "/haus/static/css/main.87ad2e7f.chunk.css"
+  },
+  {
+    "revision": "a44d6612edf480e76159",
+    "url": "/haus/static/js/2.5c7c528c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/haus/static/js/2.5c7c528c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982d2f945288aef25c73",
+    "url": "/haus/static/js/main.9f4dfdce.chunk.js"
+  },
+  {
+    "revision": "5fa6b34678216e770207",
+    "url": "/haus/static/js/runtime-main.6acebc5e.js"
+  },
+  {
+    "revision": "d5e252ed23dde9520a0cb00bfbf01a6e",
+    "url": "/haus/static/media/medium.d5e252ed.otf"
+  },
+  {
+    "revision": "9e0cb5983393efb290832cf2888a7499",
+    "url": "/haus/static/media/mediumItalic.9e0cb598.otf"
+  },
+  {
+    "revision": "cc4e72bde1aa38ceeb75e0c140ffa99c",
+    "url": "/haus/static/media/regular.cc4e72bd.otf"
+  },
+  {
+    "revision": "c1b472c36be832ed15b190a41b15f0bd",
+    "url": "/haus/static/media/regularItalic.c1b472c3.otf"
+  },
+  {
+    "revision": "ceec3f62f0a6da60ff39896fa1021191",
+    "url": "/haus/static/media/search.ceec3f62.svg"
+  },
+  {
+    "revision": "70ec86e38aa91f3785e6dfd5c1a996a9",
+    "url": "/haus/static/media/washington.70ec86e3.svg"
+  }
+]);

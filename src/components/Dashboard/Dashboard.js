32fdd99e4d3d3cb4992b01b46d9fb2b9e53fc7f1
velin13 <velin@ucsd.edu>
2020-02-09 23:24:48 -0800

    Change scrollbar and colors

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,18 +1,23 @@
 import React from 'react';
-
 import Text from '../Text/Text';
+import Grade from '../Grade/Grade';
 import styles from './dashboard.module.scss';
+import favorite from '../../assets/img/favorite.svg';
 
 const Dashboard = () => {
   return (
     <div className={styles.dashboard}>
       <div className={styles.dashboardNav}>
-        <Text className={styles.dashboardTitle} type="title1">
-          Westlake
-        </Text>
+        <div className={styles.dashboardTitle}>
+          <Text type="title1">Westlake</Text>
+          <Text type="small">
+            Median Price: $50,000 &nbsp; &bull; &nbsp; Population: 2,000{' '}
+          </Text>
+        </div>
+
         <Text type="title3">
           {' '}
-          Overall Haus Score:{' '}
+          Overall Haüs Score:{' '}
           <span className={styles.dashboardScore}> A+ </span>{' '}
         </Text>
       </div>
@@ -20,30 +25,66 @@ const Dashboard = () => {
         <div className={styles.dashboardReportCard}>
           <Text type="regular">
             {' '}
-            <span className={styles.dashboardScore}> A+ </span> Median Home
-            Value:{' '}
+            <Grade type="A">A+</Grade> Median Home Value{' '}
           </Text>
           <Text type="regular">
             {' '}
-            <span className={styles.dashboardScore}> A+ </span> Average Rent{' '}
+            <Grade type="B">B</Grade>Average Rent{' '}
           </Text>
           <Text type="regular">
             {' '}
-            <span className={styles.dashboardScore}> B+ </span> Appreciation
-            Score{' '}
+            <Grade type="A">A+</Grade> Appreciation Score{' '}
           </Text>
           <Text type="regular">
             {' '}
-            <span className={styles.dashboardScore}> A+ </span> Schools{' '}
+            <Grade type="C">C</Grade> Schools{' '}
           </Text>
           <Text type="regular">
-            <span className={styles.dashboardScore}> A+ </span> Population{' '}
+            <Grade type="D">D</Grade> Population{' '}
           </Text>
           <Text type="regular">
-            <span className={styles.dashboardScore}> C+ </span> Diversity{' '}
+            <Grade type="A">A+</Grade> Diversity{' '}
           </Text>
           <Text type="regular">
-            <span className={styles.dashboardScore}> A+ </span> Walk Score{' '}
+            <Grade type="F">F</Grade> Walk Score{' '}
+          </Text>
+        </div>
+
+        <div className={styles.dashboardStats}>
+          <Text type="title2" className={styles.dashboardCategoryTitle}>
+            About the Residents
+          </Text>
+          <div className={styles.dashboardResident}>
+            <Text type="large" className={styles.dashboardCategoryTitle}>
+              Average Median Salary
+            </Text>
+            <div className={styles.dashboardParts}></div>
+
+            <Text type="large" className={styles.dashboardCategoryTitle}>
+              Educational Diversity
+            </Text>
+          </div>
+
+          <Text type="title2" className={styles.dashboardCategoryTitle}>
+            About the Homes
+          </Text>
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Appreciation
+          </Text>
+          <div className={styles.dashboardParts}></div>
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Rent vs. Owned Households
+          </Text>
+
+          <Text type="title2" className={styles.dashboardCategoryTitle}>
+            About the Neighborhood
+          </Text>
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Schools
+          </Text>
+          <div className={styles.dashboardParts}></div>
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Crime and Safety
           </Text>
         </div>
 

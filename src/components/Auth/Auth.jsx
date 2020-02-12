96251795ae4403f0f82f060906cbfaeaa227ef20
Karthik Manickam <karthik.omanickam@gmail.com>
2020-02-11 17:54:20 -0800

    Minor file extension refactor

---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Text from 'components/Text/Text';
+import Login from 'components/Auth/Login/Login';
+
+const AuthPage = () => {
+  return (
+    <div>
+      <Text type="title1">Auth Page</Text>
+      <Login />
+    </div>
+  );
+};
+
+export default AuthPage;

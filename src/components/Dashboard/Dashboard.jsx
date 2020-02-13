469683eb76307c509888829053de00fe498b12c5
Karthik Manickam <karthik.omanickam@gmail.com>
2020-02-12 18:56:25 -0800

    Dashboard width and padding fixes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,71 +3,98 @@ import Text from '../Text/Text';
 import { Link } from 'react-router-dom';
 import Grade from '../Grade/Grade';
 import styles from './dashboard.module.scss';
-import BarChart from 'components/Dashboard/BarChart'
-import PieChart from 'components/Dashboard/PieChart'
-import LineChart from 'components/Dashboard/LineChart'
-import DoubleBarChart from 'components/Dashboard/DoubleBarChart'
+import BarChart from 'components/Dashboard/BarChart';
+import PieChart from 'components/Dashboard/PieChart';
+import LineChart from 'components/Dashboard/LineChart';
+import DoubleBarChart from 'components/Dashboard/DoubleBarChart';
 import add from 'assets/img/plus.svg';
 const SCHOOLS = [
-    { name: 'Ballard High School', rank: '27' },
-    { name: 'Center High School', rank: '1' },
-    { name: 'Cleveland High School', rank: '92' },
-    { name: 'Center School', rank: '74' },
-
-]
+  { name: 'Ballard High School', rank: '27' },
+  { name: 'Center High School', rank: '1' },
+  { name: 'Cleveland High School', rank: '92' },
+  { name: 'Center School', rank: '74' }
+];
 const Dashboard = ({ dashboardTitle, reportCard, data }) => {
-    return (
-        <div className={styles.dashboard}>
-            <div className={styles.dashboardNav}>
-                <div className={styles.dashboardTitle} >
-                    <Text type="title1">{dashboardTitle.name}</Text>
-                    <Text type="small">Median Price: ${dashboardTitle.price} &nbsp; &bull; &nbsp; Population: {dashboardTitle.population} </Text>
-                </div>
-               
-                <Link className={styles.plus}>
-                    <img
-                        className={styles.add}
-                        src={add}
-                        height={35}
-                        alt="add"
-                    />
-                </Link>
-            </div>
-            <div className={styles.dashboardContainer}>
-            <Text type="title3"> Overall Haüs Score: <span className={styles.dashboardScore}> {dashboardTitle.overall} </span> </Text>
-                <div className={styles.dashboardReportCard}>
-                    {reportCard.map(({ name, score, type }) => (
-                        <Text type="regular"> <Grade type={type}>{score}</Grade> {name} </Text>
-                    ))}
-                </div>
+  return (
+    <div className={styles.dashboard}>
+      <div className={styles.dashboardNav}>
+        <div className={styles.dashboardTitle}>
+          <Text type="title1">{dashboardTitle.name}</Text>
+          <Text type="small">
+            Median Price: ${dashboardTitle.price} &nbsp; &bull; &nbsp;
+            Population: {dashboardTitle.population}{' '}
+          </Text>
+        </div>
 
-                <div className={styles.dashboardStats}>
-                    <Text type="title2" className={styles.dashboardCategoryTitle}>About the Residents</Text>
-                        <Text type="large" className={styles.dashboardCategoryTitle}>Average Median Salary</Text>
-                        <BarChart barData={data.barData} type='0'></BarChart>
-                        <Text type="large" className={styles.dashboardCategoryTitle}>Educational Diversity</Text>
-                        <PieChart pieData={data.pieData} type='0'></PieChart>
+        <Link className={styles.plus}>
+          <img className={styles.add} src={add} height={35} alt="add" />
+        </Link>
+      </div>
+      <div className={styles.dashboardContainer}>
+        <Text type="title3">
+          {' '}
+          Overall Haüs Score:{' '}
+          <span className={styles.dashboardScore}>
+            {' '}
+            {dashboardTitle.overall}{' '}
+          </span>{' '}
+        </Text>
+        <div className={styles.dashboardReportCard}>
+          {reportCard.map(({ name, score, type }) => (
+            <Text type="regular">
+              {' '}
+              <Grade type={type}>{score}</Grade> {name}{' '}
+            </Text>
+          ))}
+        </div>
 
+        <div className={styles.dashboardStats}>
+          <Text type="title2" className={styles.dashboardCategoryTitle}>
+            About the Residents
+          </Text>
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Average Median Salary
+          </Text>
+          <BarChart barData={data.barData} type="0"></BarChart>
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Educational Diversity
+          </Text>
+          <PieChart pieData={data.pieData} type="0"></PieChart>
 
-                    <Text type="title2" className={styles.dashboardCategoryTitle}>About the Homes</Text>
-                    <Text type="large" className={styles.dashboardCategoryTitle}>Appreciation</Text>
-                        <LineChart lineData={data.lineData}></LineChart>
-                    <Text type="large" className={styles.dashboardCategoryTitle}>Rent vs. Owned Households</Text>
-                    <PieChart pieData={data.rentOwned} type='1'></PieChart>
-                    <Text type="title2" className={styles.dashboardCategoryTitle}>About the Neighborhood</Text>
-                    <Text type="large" className={styles.dashboardCategoryTitle}>Schools</Text>
-                    <Text type="small" className={styles.dashboardCategoryTitle}>Top ranked schools in this city's neighborhood</Text>
-                    {SCHOOLS.map(({ name, rank }) => (
-                        <Text type="regular"> <Grade type='A'>#{rank}</Grade> {name} </Text>
-                    ))}
-                    <Text type="large" className={styles.dashboardCategoryTitle}>Crime and Safety</Text>
-                    <DoubleBarChart barData={data.crimeData}></DoubleBarChart>
-                </div>
-
-            </div>
-
-        </div >
-    );
+          <Text type="title2" className={styles.dashboardCategoryTitle}>
+            About the Homes
+          </Text>
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Appreciation
+          </Text>
+          <LineChart lineData={data.lineData}></LineChart>
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Rent vs. Owned Households
+          </Text>
+          <PieChart pieData={data.rentOwned} type="1"></PieChart>
+          <Text type="title2" className={styles.dashboardCategoryTitle}>
+            About the Neighborhood
+          </Text>
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Schools
+          </Text>
+          <Text type="small" className={styles.dashboardCategoryTitle}>
+            Top ranked schools in this city's neighborhood
+          </Text>
+          {SCHOOLS.map(({ name, rank }) => (
+            <Text type="regular">
+              {' '}
+              <Grade type="A">#{rank}</Grade> {name}{' '}
+            </Text>
+          ))}
+          <Text type="large" className={styles.dashboardCategoryTitle}>
+            Crime and Safety
+          </Text>
+          <DoubleBarChart barData={data.crimeData}></DoubleBarChart>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -6,7 +6,12 @@ import Listings from 'components/Listings';
 import Text from 'components/Text';
 import Map from 'components/Map';
 
-const dashboardTitle = {name: 'WestLake', price: '50,000', population: '2,000', overall: 'A+'}
+const dashboardTitle = {
+  name: 'WestLake',
+  price: '50,000',
+  population: '2,000',
+  overall: 'A+'
+};
 const reportCard = [
   { name: 'Median Home Value', score: 'A+', type: 'A' },
   { name: 'Average Rent', score: 'B+', type: 'B' },
@@ -14,23 +19,27 @@ const reportCard = [
   { name: 'Schools', score: 'C+', type: 'C' },
   { name: 'Population', score: 'A+', type: 'A' },
   { name: 'Diversity', score: 'D+', type: 'D' },
-  { name: 'Walk Score', score: 'F+', type: 'F' },
-]
-const _data = { 
-  barData: [79565, 55322], 
-  pieData: [42.2, 26.5, 17.1, 3.2, 11], 
+  { name: 'Walk Score', score: 'F+', type: 'F' }
+];
+const _data = {
+  barData: [79565, 55322],
+  pieData: [42.2, 26.5, 17.1, 3.2, 11],
   lineData: [200000, 250000, 400000, 420000, 500000, 380000, 600000],
-  crimeData: [[330, 5582], [463, 2784]],
+  crimeData: [
+    [330, 5582],
+    [463, 2784]
+  ],
   rentOwned: [53, 47]
-}
+};
 const DiscoverGrid = styled.div`
   display: grid;
   padding: 2rem 2rem 4rem;
   max-width: 1360px;
   margin: auto;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 2fr;
   grid-template-rows: 1fr 10fr;
   align-items: center;
+  grid-column-gap: 15rem;
   justify-items: center;
 `;
 const LISTINGS = [
@@ -74,8 +83,11 @@ const Discover = () => {
       <Text type="title1">Seattle</Text>
       <Text type="title1">Neighborhoods</Text>
       <Map />
-      <Listings listings={LISTINGS} />
-      <Dashboard dashboardTitle={dashboardTitle} reportCard={reportCard} data={_data}></Dashboard>
+      <Dashboard
+        dashboardTitle={dashboardTitle}
+        reportCard={reportCard}
+        data={_data}
+      ></Dashboard>
     </DiscoverGrid>
   );
 };

7feb84ebb3b4519018fb8c0ec106d9bff1f4c281
Karthik Manickam <karthik.omanickam@gmail.com>
2020-02-12 16:19:12 -0800

    Refactored listings object model

--- src/components/Listings/ListingItem/ListingItem.jsx
+++ src/components/Listings/ListingItem/ListingItem.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 
 import Text from 'components/Text';
 
-const ListingItem = ({ name, scores }) => (
-  <div>
-    <Text type="large">{name}</Text>
-  </div>
-);
+const ListingItem = ({ name, scores }) => {
+  return (
+    <div>
+      <Text type="large">{name}</Text>
+      <div>
+        {scores.map(({ name, value }) => (
+          <span>
+            <Text type="large" capitalize={true}>
+              {name}:
+            </Text>
+            <Text type="regular" capitalize={true}>
+              {value}
+            </Text>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default ListingItem;

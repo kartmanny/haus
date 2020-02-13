31746178e4112a177eb1686e2bcc00aea504634a
Karthik Manickam <karthik.omanickam@gmail.com>
2020-02-12 17:53:51 -0800

    Added Logo to assets and created Grade component

---
+++ src/components/Grade/Grade.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import cx from 'classnames';
+
+import styled from 'styled-components';
+import styles from 'components/Grade/grade.module.scss';
+
+const Circle = styled.span`
+  font-size: 2rem;
+  color: white;
+  font-weight: 500;
+  border-radius: 50%;
+  height: 4rem;
+  display: inline-block;
+  width: 4rem;
+  background-color: var(--seed-color-primary);
+  text-align: center;
+  margin: 0 1rem;
+`;
+
+const Grade = ({ value }) => (
+  <Circle>
+    <span className={cx(styles.gradeText, value.length > 1 && styles.offset)}>
+      {value}
+    </span>
+  </Circle>
+);
+
+export default Grade;
--- src/components/Listings/ListingItem/ListingItem.jsx
+++ src/components/Listings/ListingItem/ListingItem.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Text from 'components/Text';
+import Grade from 'components/Grade';
+
+const Listing = styled.div`
+  padding: 2.5rem;
+  border: 2px solid var(--seed-border-light);
+  border-radius: 1rem;
+  margin: 1rem auto 1rem 0;
+  width: 100%;
+`;
+
+const Scores = styled.div`
+  margin-top: 2rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
 
 const ListingItem = ({ name, scores }) => {
   return (
-    <div>
-      <Text type="large">{name}</Text>
-      <div>
+    <Listing>
+      <Text type="title2">{name}</Text>
+      <Scores>
         {scores.map(({ name, value }) => (
           <span>
             <Text type="large" capitalize={true}>
               {name}:
             </Text>
-            <Text type="regular" capitalize={true}>
-              {value}
-            </Text>
+            <Grade value={value} />
           </span>
         ))}
-      </div>
-    </div>
+      </Scores>
+    </Listing>
   );
 };
 

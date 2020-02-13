73774497bcd9b6ef3797b24934ad3a0cb9f22c2f
Karthik Manickam <karthik.omanickam@gmail.com>
2020-02-12 18:44:58 -0800

    Added listings to profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Text from 'components/Text';
+import Listings from 'components/Listings';
 import Map from 'components/Map';
 
 const Profile = styled.div`
@@ -16,11 +17,51 @@ const ProfileGrid = styled.div`
   grid-gap: 5rem;
 `;
 
+const ProfileGridCell = styled.div`
+  display: grid;
+  grid-template-rows: 5rem 1fr;
+  justify-items: center;
+`;
+
+const FAVORITES = [
+  {
+    name: 'Queen Anne',
+    scores: [
+      { name: 'score', value: 'A+' },
+      { name: 'schools', value: 'A' },
+      { name: 'population', value: 'A-' }
+    ]
+  },
+  {
+    name: 'West Queen Anne',
+    scores: [
+      { name: 'score', value: 'B+' },
+      { name: 'schools', value: 'B' },
+      { name: 'population', value: 'B-' }
+    ]
+  },
+  {
+    name: 'South Lake Union',
+    scores: [
+      { name: 'score', value: 'C+' },
+      { name: 'schools', value: 'C' },
+      { name: 'population', value: 'C-' }
+    ]
+  }
+];
+
 const ProfileComponent = () => (
   <Profile>
-    <Text type="title1">Welcome, User</Text>
+    <Text type="title1">Welcome, Kart</Text>
     <ProfileGrid>
-      <Map />
+      <ProfileGridCell>
+        <Text type="title2">Seattle</Text>
+        <Map />
+      </ProfileGridCell>
+      <ProfileGridCell>
+        <Text type="title2">Favorited Neighborhoods</Text>
+        <Listings listings={FAVORITES} />
+      </ProfileGridCell>
     </ProfileGrid>
   </Profile>
 );

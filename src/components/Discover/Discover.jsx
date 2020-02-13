db84f3b8d3133d70b08890d6b6363285b977ef12
Karthik Manickam <karthik.omanickam@gmail.com>
2020-02-12 18:56:25 -0800

    Dashboard width and padding fixes

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Listings from 'components/Listings';
+import Dashboard from 'components/Dashboard';
 import Text from 'components/Text';
 import Map from 'components/Map';
 
-<<<<<<< HEAD
-=======
-const dashboardTitle = {name: 'WestLake', price: '50,000', population: '2,000', overall: 'A+'}
+const dashboardTitle = {
+  name: 'WestLake',
+  price: '50,000',
+  population: '2,000',
+  overall: 'A+'
+};
+
 const reportCard = [
   { name: 'Median Home Value', score: 'A+', type: 'A' },
   { name: 'Average Rent', score: 'B+', type: 'B' },
@@ -15,24 +20,28 @@ const reportCard = [
   { name: 'Schools', score: 'C+', type: 'C' },
   { name: 'Population', score: 'A+', type: 'A' },
   { name: 'Diversity', score: 'D+', type: 'D' },
-  { name: 'Walk Score', score: 'F+', type: 'F' },
-]
-const _data = { 
-  barData: [79565, 55322], 
-  pieData: [42.2, 26.5, 17.1, 3.2, 11], 
+  { name: 'Walk Score', score: 'F+', type: 'F' }
+];
+const _data = {
+  barData: [79565, 55322],
+  pieData: [42.2, 26.5, 17.1, 3.2, 11],
   lineData: [200000, 250000, 400000, 420000, 500000, 380000, 600000],
-  crimeData: [[330, 5582], [463, 2784]],
+  crimeData: [
+    [330, 5582],
+    [463, 2784]
+  ],
   rentOwned: [53, 47]
-}
->>>>>>> Allow passing of data for graphs
+};
+
 const DiscoverGrid = styled.div`
   display: grid;
   padding: 2rem 2rem 4rem;
   max-width: 1360px;
   margin: auto;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 2fr;
   grid-template-rows: 1fr 10fr;
   align-items: center;
+  grid-column-gap: 15rem;
   justify-items: center;
 `;
 const LISTINGS = [
@@ -73,18 +82,14 @@ const LISTINGS = [
 const Discover = () => {
   return (
     <DiscoverGrid>
-<<<<<<< HEAD
       <Text type="title1">Seattle</Text>
       <Text type="title1">Neighborhoods</Text>
       <Map />
-=======
-      <DiscoverContainer>
-        <Text type="title1">Seattle</Text>
-        <Map />
-      </DiscoverContainer>
-      <Dashboard dashboardTitle={dashboardTitle} reportCard={reportCard} data={_data}></Dashboard>
->>>>>>> Allow passing of data for graphs
-      <Listings listings={LISTINGS} />
+      <Dashboard
+        dashboardTitle={dashboardTitle}
+        reportCard={reportCard}
+        data={_data}
+      />
     </DiscoverGrid>
   );
 };
